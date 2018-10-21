0569a5ed49d8691047c0317c2b2b4160a08eed83
Chaitanya Hazarey <c@24.io>
2018-10-20 21:00:59 -0700

    Autoencoder setup

--- launch.sh
+++ launch.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/env bash
 
+
+
+#source pyenv/bin/activate
+
+eval "$(pyenv init -)"
+eval "$(pyenv virtualenv-init -)"
+
+pip install --upgrade pip
 pip install -r requirements.txt
-source pyenv/bin/activate
+
